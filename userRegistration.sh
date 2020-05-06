9637834e98f658ef48b87e2ebc33a2be45bd9d57
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-06 14:21:30 +0530

    UC Two Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
 
-read -p "Enter User Name : " name
-pattern="[A-Z][a-z]{2,}$"
-if [[ $name =~ $pattern ]]
+read -p "Enter User First Name : " firstName
+read -p "Enter User Last Name : " lastName
+
+firstNamePattern="[A-Z][a-z]{2,}$"
+lastNamePattern="[A-z][a-z]{2,}$"
+
+if [[ $firstName =~ $firstNamePattern ]]
 then
 	echo "Valid User Name"
 else
 	echo "Not a Valid User Name"
 fi
+
+if [[ $lastName =~ $lastNamePattern ]]
+then
+        echo "Valid User Name"
+else
+        echo "Not a Valid User Name"
+fi
+
